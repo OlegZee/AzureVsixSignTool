eab41d0dd1cba3e094ca3a5f619e690f9b905512
Oleg Zee <olegzzzz@gmail.com>
2023-12-26 15:49:07 +0100

    renamed package to AzureVsixSignTool, added more commands (samples) to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
 dotnet restore
 
-dotnet test ./tests/OpenVsixSignTool.Core.Tests/OpenVsixSignTool.Core.Tests.csproj
+dotnet build ./src/OpenVsixSignTool.Core
+dotnet build ./src/OpenVsixSignTool
+
 dotnet test ./tests/OpenVsixSignTool.Tests/OpenVsixSignTool.Tests.csproj
+dotnet test ./tests/OpenVsixSignTool.Core.Tests/OpenVsixSignTool.Core.Tests.csproj
+
+dotnet build -c Release ./src/OpenVsixSignTool
+dotnet pack -c Release ./src/OpenVsixSignTool
+
+dotnet nuget push ./src/OpenVsixSignTool/nupkg/AzureVsixSignTool.0.1.1.nupkg --source https://www.nuget.org/api/v2/package --api-key oy2i3kpla...
